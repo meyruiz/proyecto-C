b2648b97d8ad8c5cc64414bee8184cb720f4ee35
Mayra Ruiz <MayraRuiz@MacBook-Pro-de-Mayra.local>
2014-10-19 23:12:08 -0500

    Initial Commit

---
+++ jugandoCon-C/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jugandoCon-C
+//
+//  Created by Mayra Ruiz on 19/10/14.
+//  Copyright (c) 2014 Mayra Ruiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
